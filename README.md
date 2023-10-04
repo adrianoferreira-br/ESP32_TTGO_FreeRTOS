6ed000da1d58d00aa03f76611fbdc62418e2f629
adrianoferreira-br <adriano.ferreira.br@gmail.com>
2023-10-04 14:27:38 -0300

    Include new files for display, restructuring the files

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # ESP32_TTGO_FreeRTOS
 
-![ESP32_TTGO_T-Display](C:\Users\Adriano\03-Projetos\ESP32_TTGO_FreeRTOS\doc\pinmap_t-display.)
+![ESP32_TTGO_T-Display](doc/pinmap_t-display.jpg)
 
 
-Testes de estudo com ESP32 + FreeRTOS 
+* Testes de estudo com ESP32 + FreeRTOS *
+
+
+Informação da placa:
+https://www.smartkits.com.br/ttgo-t-display-v1-1-esp32-com-display-ips-1-14-colorido-16mb
+
+Informação do PlatformIO usando FreeRTOS:
+https://docs.platformio.org/en/latest/tutorials/index.html
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,59 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h>
+#include "main.h"
+#include "display.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 
-
-TFT_eSPI tft = TFT_eSPI();
 TaskHandle_t task_handle1 = NULL;
 TaskHandle_t task_handle2 = NULL;
-
-
-void vTask1(void *pvParameters);
-void vTask2(void *pvParameters);
-
-// put function declarations here:
-//#define LED_PIN 2
-
-
 
 
 void setup() {
   // put your setup code here, to run once:
   
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-  tft.drawString("Hello World!", 0, 0, 2);  
+  init_display();
 
-
-  //Serial.begin(9600);
-  //pinMode(LED_PIN, OUTPUT);
   xTaskCreate(vTask1, "Task1", 2048, NULL, 1, &task_handle1);
   xTaskCreate(vTask2, "Task2", 2048, NULL, 1, &task_handle2);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   
-  vTaskDelay(1000);
-
+ // vTaskDelay(1000);
 }
 
 
 void vTask1(void *pvParameters)
-  {
-    //pinMode(LED_PIN, OUTPUT);
-    int x,y,textsize = 0;
-    while (1)
-    {
-      //digitalWrite(LED_PIN, HIGH);
-      //tft.drawString("0", 20, 5, 2 );      
-      tft.drawChar(x,y,'A',2,2,textsize);
-      x += textsize * 6;
-      vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-    
-  }
-
-
-void vTask2(void *pvParameters) {
-  int cont = 0;
+{   
+  char str[10] = "ABC"; 
+  char* p_str = str;
   while (1)
-  {
-    //Serial.println("Task2: " + String(cont++));
-    //tft.drawString("1", 0, 0, 2);
-    tft.drawString("1", 20, 5, 2 );      
+  { 
+    vTaskDelay(pdMS_TO_TICKS(500));     
+    displayPrint(p_str,sizeof(str)/sizeof(char),50,50);      
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+    
+}
+
+
+void vTask2(void *pvParameters) 
+{  
+  while (1)
+  {    
+    displayPrint("123",3, 50, 50);      
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 
 }
+
+
+//#define LED_PIN 2
+//Serial.begin(9600);
+  //pinMode(LED_PIN, OUTPUT);
+//pinMode(LED_PIN, OUTPUT);
+//Serial.println("Task2: " + String(cont++));
+    //tft.drawString("1", 0, 0, 2);
